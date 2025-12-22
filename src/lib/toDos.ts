79f4c2d2925a8f239626642de76303992aee4484
Josh Fisher <jfisherd@gmail.com>
2025-12-22 14:31:43 -0500

    define ToDo interface and add mock to-do's

---
+++ src/lib/toDos.ts
@@ -0,0 +1,25 @@
+import type { ToDo } from "../types/types"
+
+
+export const ToDo[] = [{
+    id: 1,
+    title: 'title',
+    description: 'description',
+    dateAdded: '01/01/1970',
+    dateDue: '01/01/2070',
+    completedStatus: false
+}, {
+    id: 2,
+    title: 'title',
+    description: 'description',
+    dateAdded: '01/01/1970',
+    dateDue: '01/01/2070',
+    completedStatus: false
+}, {
+    id: 3,
+    title: 'title',
+    description: 'description',
+    dateAdded: '01/01/1970',
+    dateDue: '01/01/2070',
+    completedStatus: false
+}]
---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+export interface ToDo {
+    id: number
+    title: string
+    description: string
+    dateAdded: string
+    dateDue: string
+    completedStatus: boolean
+}
