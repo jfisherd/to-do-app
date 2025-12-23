2883a3ab41bc1d4e9ccd357cacc15a0da735cc31
Josh Fisher <jfisherd@gmail.com>
2025-12-23 11:19:12 -0500

    pass props from ToDoList.tsx to ToDoListItem.tsx

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -5,14 +5,21 @@ import type { ToDo } from "../types/types"
 
 export function ToDoList() {
 
-     const [listArray, setListArray] = useState<ToDo[]>(myBigList)
+    const [listArray, setListArray] = useState<ToDo[]>(myBigList)
 
-    
+
 
     return (
         <>
             <ul>
-                {listArray.map(() => <ToDoListItem />)}
+                {listArray.map((item) => <ToDoListItem
+                    id={item.id}
+                    title={item.title}
+                    text={item.text}
+                    dateAdded={item.dateAdded}
+                    dateDue={item.dateDue}
+                    completed={item.completed}
+                />)}
             </ul>
         </>
     )
--- src/contexts/ToDoContext.ts
+++ src/contexts/ToDoContext.ts
@@ -2,37 +2,16 @@ import React from "react";
 import { createContext } from "react";
 import { ReactNode } from "react";
 
-//   function addToDo() {
-//     // add new to do item
-//   }
-
-//   function toggleToDo() {
-//     // toggle completed boolean
-//   }
-
-//   function deleteToDo() {
-//     // delete to do item
-//   }
-
-//   function editToDo() {
-//     // edit to do item
-//   }
-
-//   function clearCompleted() {
-//     // delete completed to do items
-//   }
-
-
 
 
 export const ThemeContext = React.createContext('light'); // Theme section of Lesson 5 emulated here
 
 export function ToDoContext() {
-    const addToDo = React.createContext(() => { })
-    const toggleToDo = React.createContext(() => { })
-    const deleteToDo = React.createContext(() => { })
-    const editToDo = React.createContext(() => { })
-    const clearCompleted = React.createContext(() => { })
+    const addToDo = React.createContext(() => {/* add new to do item */})
+    const toggleToDo = React.createContext(() => {/* toggle completed boolean */})
+    const deleteToDo = React.createContext(() => {/* delete to do item */})
+    const editToDo = React.createContext(() => {/* edit to do item */})
+    const clearCompleted = React.createContext(() => {/* delete completed to do items */})
 
     return (
         <></>
