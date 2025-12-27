514c1898408bf6dc23b599dc520b801101d2a078
Josh Fisher <jfisherd@gmail.com>
2025-12-27 14:19:56 -0500

    ToDoInput.tsx, add basic framework

--- src/components/ToDoInput.tsx
+++ src/components/ToDoInput.tsx
@@ -5,6 +5,14 @@ export function ToDoInput() {
     return (
         <>
             <div>To-Do Input</div>
+
+            {/* id should be assigned to new to-do automatically */}
+            <input/> Title
+            <input/> Text
+            {/* dateAdded should be assigned automatically */}
+            <input/> dateDue
+            {/* completed status should default to false */}
+
         </>
     )
 }
