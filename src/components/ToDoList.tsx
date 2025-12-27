06e3f53f217513b01944e08e460e7d9ff8bf08b0
Josh Fisher <jfisherd@gmail.com>
2025-12-27 13:58:17 -0500

    ToDoContext.ts, initialize context as empty ToDo array instead of null. ToDoList.tsx, consume context

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,7 +1,11 @@
+import { ToDoContext } from "../contexts/ToDoContext"
 import { ToDoListItem } from "./ToDoListItem"
-
+import { useContext } from "react"
 
 export function ToDoList() {
+
+    const { listArray } = useContext(ToDoContext)
+    
 
     return (
         <>
--- src/contexts/ToDoContext.ts
+++ src/contexts/ToDoContext.ts
@@ -1,5 +1,5 @@
 import React from "react";
 import type { ToDo } from "../types/types";
 
-export const ToDoContext: undefined | ToDo[] = React.createContext(undefined)
+export const ToDoContext: ToDo[] = React.createContext([] as ToDo[]);
 
